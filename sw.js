fd7ff31461b9cf77e08b516ebe7644195c9f1b88
fromtotoro77 <fromtotoro@gmail.com>
2026-03-15 10:18:22 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'k-radio-v1';
+const CACHE_NAME = 'daddy-radio-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -8,8 +8,19 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // 즉시 활성화 유도
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
   );
 });
 
